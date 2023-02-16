3df9f285018ba40d2f5b5b38dc983dff18550cdc
Harish Sivakumaran <harish.sivakumaran@sap.com>
2023-02-16 10:25:23 +0530

    spring boot

---
+++ IdeaBoard/src/main/java/com/ideaboard/IdeaBoard/IdeaBoardApplication.java
@@ -0,0 +1,13 @@
+package com.ideaboard.IdeaBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaBoardApplication.class, args);
+	}
+
+}
---
+++ IdeaBoard/src/test/java/com/ideaboard/IdeaBoard/IdeaBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideaboard.IdeaBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
